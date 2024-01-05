d5a3b2411a8a15c982c41ae43fbafac06372f482
Yahir Hernandez <hernandezyahir05@hotmail.com>
2024-01-04 22:50:57 -0700

    Main branch

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define ull unsigned long long int
+
+using namespace std;
+
+//-------------------SOLBEGIN-------------------
+
+void solve() {
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
