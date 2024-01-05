0dc4d3da3dc2d10035d6b655aeb42676eed308c7
Yahir Hernandez <hernandezyahir05@hotmail.com>
2024-01-04 23:36:44 -0700

    Main branch

---
+++ Graphs/breadth_first_search.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<bool> visited;
+vector<vector<int>> adj;
+
+void breadth_first_search(int node) {
+    // BFS requieres queue data structure, starting from initial node 0
+    queue<int> q;
+    q.push(node);
+    visited[node] = true;
+    // While queue is not empty, pop the first element and push its children
+    while (!q.empty()) {
+        int v = q.front();
+        cout << v << " ";
+        q.pop();
+        // Push all children of v
+        for (int u : adj[v]) {
+            // If not visited, push and mark as visited
+            if (!visited[u]) {
+                q.push(u);
+                visited[u] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int nodes, edges;
+    cin >> nodes >> edges;
+    // Initialize visited and adjacency list
+    visited.assign(nodes, false);
+    adj.assign(nodes, vector<int>());
+    int u, v;
+    // Values of nodes staring from 0, given as pairs of nodes
+    for (int i = 0; i < edges; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // Assuming undirected graph
+    }
+    breadth_first_search(0); // Start BFS from node 0
+
+    return 0;
+}
